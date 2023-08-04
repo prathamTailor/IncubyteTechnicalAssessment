c58d9be85c4545c4e2f0af8f4e6caa90d3314d97
prathamTailor <20ceubg054@ddu.ac.in>
2023-08-04 16:17:54 +0530

    moveForward function added
    testing of moveForward function

--- chandrayan3.js
+++ chandrayan3.js
@@ -17,7 +17,79 @@ class ChandraYan3{
         this.currDirection = currDirection;
         this.galaxy = galaxy;
     }
+
+    moveForward(){
+        if(this.currDirection == 'n'){
+            if(this.y+1 <= this.galaxy.ypBoundry){
+                this.y += 1;
+            }else{
+                throw Error("Chandrayan move out of y boundry of galaxy");
+            }
+        }else if(this.currDirection == 's'){
+            if(this.y-1 >= -1*this.galaxy.ynBoundry){
+                this.y -= 1;
+            }else{
+                throw Error("Chandrayan move out of y boundry of galaxy");
+            }
+        } else if(this.currDirection == 'e'){
+            if(this.x+1 <= this.galaxy.xpBoundry){
+                this.x += 1;
+            }else{
+                throw Error("Chandrayan move out of x boundry of galaxy");
+            }
+        }else if(this.currDirection == 'w'){
+            if(this.x-1 >= -1*this.galaxy.xnBoundry){
+                this.x -= 1;
+            }else{
+                throw Error("Chandrayan move out of x boundry of galaxy");
+            }
+        }else if(this.currDirection == 'u'){
+            if(this.z+1 <= this.galaxy.zpBoundry){
+                this.z += 1;
+            }else{
+                throw Error("Chandrayan move out of z boundry of galaxy");
+            }
+        }else if(this.currDirection == 'd'){
+            if(this.z-1 >= -1*this.galaxy.znBoundry){
+                this.z -= 1;
+            }
+            else{
+                throw Error("Chandrayan move out of z boundry of galaxy");
+            }
+        }
+    }
+
+    printCurrStat(){
+        console.log("( "+this.x+", "+this.y+", "+this.z+", "+this.currDirection+" )");
+    }
+
+    moveChandrayan(command){
+        command = command.toLowerCase();
+        switch(command){
+            case 'f':
+                this.moveForward();
+                break;  
+            default:
+                throw Error("Invalid command");
+        }
+    }
 }
+
+// commands = "fff";
+
+// var galaxy = new Galaxy(3,3,3,3,3,3);
+// var rocket = new ChandraYan3(0,0,0,'n',galaxy);
+
+// console.log("Initial state: ");
+// rocket.printCurrStat();
+// console.log();
+
+// for(indx in commands){
+//     rocket.moveChandrayan(commands[indx]);
+// }
+
+// console.log("Final state: ");
+// rocket.printCurrStat();
 
 module.exports = {
     Galaxy: Galaxy,
--- test.spec.js
+++ test.spec.js
@@ -1,6 +1,6 @@
 const{Galaxy,ChandraYan3} = require("./chandrayan3");
 
-describe('Initialization of classes', () => {
+describe('Testing of chandrayan', () => {
     let galaxy;
     let chandrayan3;
 
@@ -20,6 +20,40 @@ describe('Initialization of classes', () => {
         expect(chandrayan3.galaxy.xnBoundry).toBe(3);
         expect(chandrayan3.galaxy.ynBoundry).toBe(3);
         expect(chandrayan3.galaxy.znBoundry).toBe(3);
+    });
+
+    test('Testing moveForward function with invalid command', () => {
+        command = "zp";
+        try{
+            chandrayan3.moveChandrayan(command);
+        }catch(e){
+            expect(e.message).toEqual("Invalid command");
+        }
+    });
+
+    test('Testing moveForward function with correct command', () => {
+        command = "f";
+        try{
+            chandrayan3.moveChandrayan(command);
+        }catch(e){
+            expect(e.message).toEqual("");
+        }
+        expect(chandrayan3.x).toBe(0);
+        expect(chandrayan3.y).toBe(1);
+        expect(chandrayan3.z).toBe(0);
+        expect(chandrayan3.currDirection).toBe('n');
+    });
+
+    test('Testing moveForward function with correct command but out of galaxys y boundry', () => {
+        command = "f";
+        chandrayan3.y=3;
+
+        try{
+            chandrayan3.moveChandrayan(command);
+        }catch(e){
+            expect(e.message).toEqual("Chandrayan move out of y boundry of galaxy");
+        }
+        
     });
  });
  
